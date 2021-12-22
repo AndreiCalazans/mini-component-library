73901dca80d0a6005c819f7a73e0c1ea2d52f1f1
Andrei Calazans <andreixoc@hotmail.com>
2021-12-22 06:43:54 -0300

    exercise 1 - progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const sizes = {
+  large: {
+    height: 24,
+    padding: 4,
+    radius: 8,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
 };
+
+const ProgressBar = ({ value, size = "small" }) => {
+  return (
+    <BarBackground
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuetext="Step 2: Copying files... "
+      aria-valuemax="100"
+      {...sizes[size]}
+    >
+      <Bar value={value} radius={sizes[size].radius} />
+    </BarBackground>
+  );
+};
+
+const BarBackground = styled.div`
+  position: relative;
+  width: 100%;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  height: ${(props) => props.height}px;
+  padding: ${(props) => props.padding}px;
+  border-radius: ${(props) => props.radius}px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: ${(props) => props.value}%;
+  border-radius: ${(props) => {
+    if (props.value > 99.8) {
+      return `${props.radius}px`;
+    }
+
+    return `${props.radius}px 0px 0px ${props.radius}px`;
+  }};
+  height: 100%;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
